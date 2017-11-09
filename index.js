ef9e70385c54e3f6bd4a2c04fc03f0b785910e29
Brian Winter <blackpawn8990@gmail.com>
2017-11-09 13:15:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ function distanceTravelledInFeet(start, end){
 }
 function calculatesFarePrice(start) {
    distanceTravelledInFeet();
-  if (distanceTravelledInFeet() <400) {
+  if (distanceTravelledInFeet(start, end) <400) {
     return 0;
   } else {
   if (distanceTravelledInFeet() >400 && distanceTravelledInFeet() <2000) {
