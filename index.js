e1be0775d4558ec769d98afabd080c4234bfe338
Brian Winter <blackpawn8990@gmail.com>
2017-11-09 04:37:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // Code your solution in this file!
 function distanceFromHqInBlocks (destination){
-    if ( destination >= 42){
+  
       const distanceInBlocks = destination - 42;
 
       return distanceInBlocks;
-    }
+    
 }
 distanceFromHqInBlocks(34);
