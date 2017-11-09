13ec0a8f747084c97f8a0ecc7070e7dc7b683301
Brian Winter <blackpawn8990@gmail.com>
2017-11-09 03:44:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function distanceFromHqInBlocks (headquarters, destination){
     if ( destination >= headquarters){
       const block = '${destination} - ${headquarters} + 1';
 
-      return 'You have traveled ${block} blocks.';
+      return `You have traveled ${block} blocks.`;
     }
 }
 distanceFromHqInBlocks(43, 43);
