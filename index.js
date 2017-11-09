42cc920e1a2c4d9457bd5ae117cd832e05af1bf5
Brian Winter <blackpawn8990@gmail.com>
2017-11-09 14:31:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -36,15 +36,5 @@ function calculatesFarePrice(start,end) {
 
   if (x <400) {
     return 0;
-  } else {
-  if (x  >400 && x  <2000) {
-    return x * 0.02;
-  } else {
-  if (x >2000 && x <2500) {
-    return 25;
-  } else {
-    return 'cannot travel that far';
-  }
-  }
   }
   }
