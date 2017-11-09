92443c9cf119fe805c659c31d38ca7c776567306
Brian Winter <blackpawn8990@gmail.com>
2017-11-09 03:24:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solution in this file!
-function distanceFromHqInBlocks (){
-  
+function distanceFromHqInBlocks (headquarters, destination){
+     destination - 42;
 }
 function distanceFromHqInFeet(){
   distanceFromHqInBlocks();
