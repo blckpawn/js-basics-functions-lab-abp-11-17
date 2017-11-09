c791134bb3ab5ee60cec6dce7b63be2896c8d7f8
Brian Winter <blackpawn8990@gmail.com>
2017-11-09 03:29:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Code your solution in this file!
 function distanceFromHqInBlocks (headquarters, destination){
-    console.log("The driver when from headquarters to destination");
+    console.log(`The driver when from ${headquarters} to ${destination}`);
 }
 distanceFromHqInBlocks(42, 43); 
