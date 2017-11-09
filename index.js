b0f7110b694c8b11dde8273162eb13ab16c0e669
Brian Winter <blackpawn8990@gmail.com>
2017-11-09 04:58:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function distanceFromHqInBlocks (destination){
 function distanceFromHqInFeet () {
   distanceFromHqInBlocks();
 
-    const distanceInFeet = distanceFromHqInBlocks(0) * 264;
+    const distanceInFeet = distanceFromHqInBlocks(43) * 264;
 
     return distanceInFeet;
 
