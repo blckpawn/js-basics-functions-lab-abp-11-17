5dbdc47a2d594663435ace35ecd9de7155e55113
Brian Winter <blackpawn8990@gmail.com>
2017-11-09 03:48:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,4 +6,4 @@ function distanceFromHqInBlocks (headquarters, destination){
       return `You have traveled ${block} blocks.`;
     }
 }
-distanceFromHqInBlocks(43, 44);
+distanceFromHqInBlocks(42, 43);
