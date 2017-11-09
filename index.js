883ea244235de34dc3fa64082779bf9dc56fae80
Brian Winter <blackpawn8990@gmail.com>
2017-11-09 04:40:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Code your solution in this file!
 function distanceFromHqInBlocks (destination){
 
-      const distanceInBlocks = Math.abs(destination - 42);
+       Math.abs(destination - 42);
 
       return distanceInBlocks ;
 
@@ -10,5 +10,7 @@ distanceFromHqInBlocks(34);
 
 
 function distanceFromHqInFeet () {
-  distanceFromHqInBlocks()*264;
+  distanceFromHqInBlocks();
+
+   
 }
