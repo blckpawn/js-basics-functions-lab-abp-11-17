9bd9f823992077f0c6a6f08fe79428be55d630a8
Brian Winter <blackpawn8990@gmail.com>
2017-11-09 04:30:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // Code your solution in this file!
 function distanceFromHqInBlocks (headquarters, destination){
     if ( destination >= headquarters){
-      const distanceInBlocks = destination - headquarters;
+      const distanceInBlocks = destination - 42;
 
       return `${distanceInBlocks} returns a distance in blocks.`;
-    }
+    } 
 }
 distanceFromHqInBlocks(42, 43);
